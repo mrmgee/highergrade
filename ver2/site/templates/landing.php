00599cb4fa30e021a72b0858d484f5e594126a96
mrmgee <mrmgee@gmail.com>
2019-01-06 13:47:33 -0700

    Remove Westword holiday special offers

--- ver2/site/templates/landing.php
+++ ver2/site/templates/landing.php
@@ -11,9 +11,6 @@
 
             <header>
               <?= $page->intro()->kirbytext() ?>
-              
-
-            <a href="#deals" class="more scrolly">See the Specials</a>
             </header>
 
 
@@ -37,22 +34,6 @@
           <section id="one" class="spotlight">
             <div class="image"><img src="<?= $page->spot1image()->toFile()->url() ?>" alt="" /></div>
             <div id="deals" class="content">
-              <div class="feature">
-                  <h2>Recreational Holiday Deals!</h2>
-                  <ul>
-                    <li>$35 Olio Cartridges</li>
-                    <li>$25 Olio Live Resin</li>
-                    <li>$12 Craft Cured</li>
-                    <li>Gold and Silver Shelf Strains</br>
-                      $20 1/8ths & $125/oz</li>
-                    <li>Featured Strains</br>
-                    $15 1/8ths  & $99/oz out the door</li>
-                  </ul>
-                  <h6>*Now until January 5th at both stores (while supplies last)</br>
-Additional discounts will not apply</h6>
-              </div>
-
-
               <?= $page->spot1txt()->kirbytext() ?>
               <a href="<?php echo $page->button1link() ?>" class="button special"><?= $page->button1txt()->kirbytext() ?></a>
             </div>
@@ -65,20 +46,6 @@ if (strlen($page->button2txt())==0) {
           <section class="spotlight">
             <div class="image"><img src="<?= $page->spot2image()->toFile()->url() ?>" alt="" /></div>
             <div class="content">
-              <div class="feature">
-                  <h2>Medical Specials</h2>
-                  <ul>
-                    <li>$25 Olio Live Resin / $700 oz</li>
-                    <li>$13 Apothecary Cured / $360 oz</li>
-                    <li>$10 GDL & Olio Cured Concentrates / $280 oz</li>
-                    <li>$35 Olio & Harmony Live Res Cartridges</li>
-                    <li>*All Featured Strains</br>
-                      $15 1/8ths  & $95 oz</li>
-                  </ul>
-                  <h6>*Now until January 5th at both stores (while supplies last)</br>
-Additional discounts will not apply</h6>
-              </div>
-
               <?= $page->spot2txt()->kirbytext() ?>
               <a href="<?php echo $page->button2link() ?>" class="button special"><?= $page->button2txt()->kirbytext() ?></a>
             </div>
